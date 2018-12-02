ebc36952503e431dd49f8378b06f93c205bffce0
Leowario <voronventusa@gmail.com>
2018-12-02 18:35:06 +0200

    Event loop test added, access modifier changed for testing

--- src/test/java/TestApplication.java
+++ src/test/java/TestApplication.java
@@ -1,5 +1,7 @@
 import com.codeit.eventsperformer.logic.Event;
+import com.codeit.eventsperformer.logic.EventLoop;
 import com.codeit.eventsperformer.logic.EventParser;
+import com.codeit.eventsperformer.logic.EventsQueueFactory;
 import org.json.simple.parser.ParseException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -12,12 +14,22 @@ import java.net.URISyntaxException;
 class TestApplication {
 
     @Test
-    @DisplayName("parse from JSON")
-    void parseFromJson() throws ParseException, IOException, URISyntaxException {
+    @DisplayName("parse from JSON to array of Events")
+    void parseFromJsonTest() throws ParseException, IOException, URISyntaxException {
 
-        EventParser eventParser = new EventParser();
-        Event[] events = eventParser.parse();
+        Event[] events = getEvents();
         Assertions.assertEquals("01:15", events[events.length - 1].getTime());
+    }
+
+    @Test
+    @DisplayName("run event loop")
+    void eventLoopTest() throws ParseException, IOException, URISyntaxException {
+        EventLoop.instance().start(EventsQueueFactory.getQueue(getEvents()));
+    }
+
+    private Event[] getEvents() throws URISyntaxException, IOException, ParseException {
+        EventParser eventParser = new EventParser();
+        return eventParser.parse();
     }
 
 }
