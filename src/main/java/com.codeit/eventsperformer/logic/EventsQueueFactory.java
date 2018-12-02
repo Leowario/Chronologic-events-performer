536e867baca869ded4100afb44bf66d91d4a0eb5
Leowario <voronventusa@gmail.com>
2018-12-02 12:17:51 +0200

    EventsQueueFactory created

---
+++ src/main/java/com.codeit/eventsperformer/logic/EventsQueueFactory.java
@@ -0,0 +1,20 @@
+package com.codeit.eventsperformer.logic;
+
+import java.util.PriorityQueue;
+
+import static java.util.Collections.addAll;
+
+/**
+ * @author Vitalii Usatyi
+ */
+public class EventsQueueFactory {
+
+    private EventsQueueFactory(){
+
+    }
+    static PriorityQueue<Event> getQueue(Event[] events) {
+        PriorityQueue<Event> eventQueue = new PriorityQueue<>();
+        addAll(eventQueue, events);
+        return eventQueue;
+    }
+}
