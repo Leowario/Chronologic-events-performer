e66a2465351fc846f24ccf4c9c6401cb1fd3b8f8
Leowario <voronventusa@gmail.com>
2018-12-02 14:30:06 +0200

    configuration for Run added

--- src/main/java/com.codeit/eventsperformer/Main.java
+++ src/main/java/com.codeit/eventsperformer/Main.java
@@ -1,7 +1,9 @@
 package com.codeit.eventsperformer;
 
+import com.codeit.eventsperformer.logic.Application;
+
 public class Main {
     public static void main(String[] args) {
-
+        Application.instance().run();
     }
 }
