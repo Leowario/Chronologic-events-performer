e3539b057b18852daae58bdd34aa0bdf72e60ff8
Leowario <voronventusa@gmail.com>
2018-12-02 18:25:33 +0200

    parseFromJson test method created

--- src/test/java/TestApplication.java
+++ src/test/java/TestApplication.java
@@ -1,3 +1,23 @@
+import com.codeit.eventsperformer.logic.Event;
+import com.codeit.eventsperformer.logic.EventParser;
+import org.json.simple.parser.ParseException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+@DisplayName("Application should:")
 public class TestApplication {
-    
+
+    @Test
+    @DisplayName("parse from JSON")
+    public void parseFromJson() throws ParseException, IOException, URISyntaxException {
+
+        EventParser eventParser = new EventParser();
+        Event[] events = eventParser.parse();
+        Assertions.assertEquals("01:15", events[events.length - 1].getTime());
+    }
+
 }
