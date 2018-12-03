c5549a8d05211e83cc8b44a0ee9d1246849cf9f2
Leowario <voronventusa@gmail.com>
2018-12-03 11:56:27 +0200

    Log with current time of the system added

--- src/main/java/com.codeit/eventsperformer/Main.java
+++ src/main/java/com.codeit/eventsperformer/Main.java
@@ -6,6 +6,7 @@ import com.codeit.eventsperformer.logic.Application;
  * @author Vitalii Usatyi
  */
 public class Main {
+    public static final long startTime = System.currentTimeMillis();
     public static void main(String[] args) {
         Application.instance().run();
     }
